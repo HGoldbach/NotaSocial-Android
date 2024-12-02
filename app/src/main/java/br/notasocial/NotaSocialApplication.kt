c6d90bb0f11ed350e6e82d20c1198ce211bdb61c
HGoldbach <goldbachcs@gmail.com>
2024-12-02 01:34:30 -0300

    update application

--- app/src/main/java/br/notasocial/NotaSocialApplication.kt
+++ app/src/main/java/br/notasocial/NotaSocialApplication.kt
@@ -1,14 +1,25 @@
 package br.notasocial
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import br.notasocial.data.AppContainer
 import br.notasocial.data.DefaultAppContainer
+import br.notasocial.data.repository.UserPreferencesRepository
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "settings"
+)
 
 class NotaSocialApplication : Application() {
     lateinit var container: AppContainer
+    lateinit var userPreferencesRepository: UserPreferencesRepository
 
     override fun onCreate() {
         super.onCreate()
+        userPreferencesRepository = UserPreferencesRepository(dataStore)
         container = DefaultAppContainer(this)
     }
 }
